55fc926b3e671093b5be6741aaee1d4f3d8711f4
Benjamin Letzel <benjamin.letzel@protonmail.com>
2026-04-16 20:41:08 +0200

    + adjust service.php to use symfony's autowiring

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -1,20 +1,31 @@
 <?php declare(strict_types=1);
 
 use Fib\ProductLabel\Core\Content\ProductLabel\Aggregate\ProductLabelProduct\ProductLabelProductDefinition;
+use Fib\ProductLabel\Core\Content\ProductLabel\Cache\ProductLabelCacheInvalidationSubscriber;
 use Fib\ProductLabel\Core\Content\ProductLabel\Commands\DeactivateExpiredLabelCommand;
 use Fib\ProductLabel\Core\Content\ProductLabel\Extension\ProductLabelProductExtension;
 use Fib\ProductLabel\Core\Content\ProductLabel\ProductLabelDefinition;
 use Fib\ProductLabel\Core\Content\ProductLabel\ProductLabelTranslationDefinition;
-use Fib\ProductLabel\Core\Content\ProductLabel\Service\ProductLabelVisibilityService;
 use Fib\ProductLabel\Core\Content\ProductLabel\ScheduledTask\DeactivateExpiredLabelsTask;
 use Fib\ProductLabel\Core\Content\ProductLabel\ScheduledTask\DeactivateExpiredLabelsTaskHandler;
 use Fib\ProductLabel\Core\Content\ProductLabel\Service\DeactivateExpiredLabelsService;
+use Fib\ProductLabel\Core\Content\ProductLabel\Service\ProductLabelVisibilityService;
+use Fib\ProductLabel\Storefront\Page\ProductLabelPageDataLoader;
 use Fib\ProductLabel\Storefront\Subscriber\ProductLabelCriteriaSubscriber;
+use Fib\ProductLabel\Storefront\Subscriber\ProductLabelListingSubscriber;
+use Fib\ProductLabel\Storefront\Subscriber\ProductLabelPageSubscriber;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
+
+    $services
+        ->defaults()
+        ->autowire()
+        ->autoconfigure(false)
+        ->public(false);
 
     $services->set(ProductLabelDefinition::class)
         ->tag('shopware.entity.definition', ['entity' => 'fib_product_label']);
@@ -31,30 +42,31 @@ return static function (ContainerConfigurator $container): void {
     $services->set(ProductLabelVisibilityService::class);
 
     $services->set(DeactivateExpiredLabelsService::class)
-        ->args([
-            service('fib_product_label.repository'),
-        ]);
+        ->arg('$productLabelRepository', service('fib_product_label.repository'));
 
     $services->set(DeactivateExpiredLabelCommand::class)
-        ->args([
-            service(DeactivateExpiredLabelsService::class),
-        ])
         ->tag('console.command');
 
     $services->set(ProductLabelCriteriaSubscriber::class)
-        ->args([
-            service(ProductLabelVisibilityService::class),
-        ])
         ->tag('kernel.event_subscriber');
 
     $services->set(DeactivateExpiredLabelsTask::class)
         ->tag('shopware.scheduled.task');
 
     $services->set(DeactivateExpiredLabelsTaskHandler::class)
-        ->args([
-            service('scheduled_task.repository'),
-            service('logger'),
-            service(DeactivateExpiredLabelsService::class),
-        ])
+        ->arg('$scheduledTaskRepository', service('scheduled_task.repository'))
+        ->arg('$logger', service('logger'))
         ->tag('messenger.message_handler');
+
+    $services->set(ProductLabelPageDataLoader::class);
+
+    $services->set(ProductLabelPageSubscriber::class)
+        ->tag('kernel.event_subscriber');
+
+    $services->set(ProductLabelListingSubscriber::class)
+        ->tag('kernel.event_subscriber');
+
+    $services->set(ProductLabelCacheInvalidationSubscriber::class)
+        ->arg('$mappingRepository', service('fib_product_label_product.repository'))
+        ->tag('kernel.event_subscriber');
 };
